ddebdea51e904b8f15a58ffae7d66ce6ac416bab
unknown <anhvv_57@vnu.edu.vn>
2014-03-22 22:10:56 +0700

    fvf

--- src/GUI/SupplierManager/SupplierRow.java
+++ src/GUI/SupplierManager/SupplierRow.java
@@ -14,12 +14,12 @@ import GUI.Classes.Configure;
 import GUI.Classes.CustomButton;
 import GUI.Classes.CustomFont;
 import GUI.Classes.CustomLabel;
-import GUI.MedicinesManager.MedicineRow;
-import static GUI.MedicinesManager.MedicineRow.white;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SupplierRow extends JPanel {
         public static boolean white = true;
-	public SupplierRow(String id, String name, String type, String address, String phone, int idSize, int nameSize, int typeSize, int addressSize, int phoneSize, int optionSize, Point pos, JPanel parentPanel) {
+	public SupplierRow(String id, String name, String email, String address, String phone, int idSize, int nameSize, int emailSize, int addressSize, int phoneSize, int optionSize, Point pos, JPanel parentPanel) {
 		super();
 		setLayout(null);
 		Color BackGround = null;
@@ -27,19 +27,36 @@ public class SupplierRow extends JPanel {
                 else BackGround = Color.getHSBColor(20, 12, 21);
                 setBackground(BackGround);
                 white = !white;
-		setBounds(pos.x, pos.y, idSize + nameSize + typeSize + addressSize + phoneSize + optionSize, 40);
+		setBounds(pos.x, pos.y, idSize + nameSize + emailSize + addressSize + phoneSize + optionSize, 40);
 		CustomLabel idLabel = new CustomLabel(id, Color.GRAY, Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 13), new Point(pos.x, pos.y), new Dimension(idSize, 40), true, SwingConstants.CENTER, SwingConstants.CENTER, parentPanel);
 		CustomLabel nameLabel = new CustomLabel(name, Color.GRAY, Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 13), new Point(pos.x + idSize, pos.y), new Dimension(nameSize, 40), true, SwingConstants.CENTER, SwingConstants.CENTER, parentPanel);
-		CustomLabel typeLabel = new CustomLabel(type, Color.GRAY, Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 13), new Point(pos.x + idSize + nameSize, pos.y), new Dimension(typeSize, 40), true, SwingConstants.CENTER, SwingConstants.CENTER, parentPanel);
-		CustomLabel addressLabel = new CustomLabel(address, Color.GRAY, Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 13), new Point(pos.x + idSize + typeSize + nameSize, pos.y), new Dimension(addressSize, 40), true, SwingConstants.CENTER, SwingConstants.CENTER, parentPanel);
-		CustomLabel ramainLabel = new CustomLabel(phone, Color.GRAY, Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 13), new Point(pos.x +idSize + typeSize + addressSize + nameSize, pos.y), new Dimension(phoneSize, 40), true, SwingConstants.CENTER, SwingConstants.CENTER, parentPanel);
-		CustomButton devider = new CustomButton("", Color.WHITE, null, false, false, Color.LIGHT_GRAY, true, new Point(pos.x, pos.y + 39), new Dimension(idSize + nameSize + typeSize + addressSize + phoneSize + optionSize , 1), parentPanel);
+		CustomLabel emailLabel = new CustomLabel(email, Color.GRAY, Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 13), new Point(pos.x + idSize + nameSize, pos.y), new Dimension(emailSize, 40), true, SwingConstants.CENTER, SwingConstants.CENTER, parentPanel);
+		CustomLabel addressLabel = new CustomLabel(address, Color.GRAY, Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 13), new Point(pos.x + idSize + emailSize + nameSize, pos.y), new Dimension(addressSize, 40), true, SwingConstants.CENTER, SwingConstants.CENTER, parentPanel);
+		CustomLabel phoneLabel = new CustomLabel(phone, Color.GRAY, Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 13), new Point(pos.x +idSize + emailSize + addressSize + nameSize, pos.y), new Dimension(phoneSize, 40), true, SwingConstants.CENTER, SwingConstants.CENTER, parentPanel);
+		CustomButton devider = new CustomButton("", Color.WHITE, null, false, false, Color.LIGHT_GRAY, true, new Point(pos.x, pos.y + 39), new Dimension(idSize + nameSize + emailSize + addressSize + phoneSize + optionSize , 1), parentPanel);
 		
-		CustomButton details = new CustomButton(new ImageIcon("src/GUI/Resources/details.bin"), "", Color.WHITE, null, false, false, BackGround, true, new Point(idSize + nameSize + typeSize + addressSize + phoneSize + (optionSize-80)/2, 5), new Dimension(20, 30), SupplierRow.this,SwingConstants.CENTER, SwingConstants.CENTER);
+		CustomButton details = new CustomButton(new ImageIcon("src/GUI/Resources/details.bin"), "", Color.WHITE, null, false, false, BackGround, true, new Point(idSize + nameSize + emailSize + addressSize + phoneSize + (optionSize-80)/2, 5), new Dimension(20, 30), SupplierRow.this,SwingConstants.CENTER, SwingConstants.CENTER);
 		details.setRolloverIcon(new ImageIcon("src/GUI/Resources/detailsRollover.bin"));
-		CustomButton edit = new CustomButton(new ImageIcon("src/GUI/Resources/edit.bin"), "", Color.WHITE, null, false, false, BackGround, true, new Point(idSize + nameSize + typeSize + addressSize + phoneSize + (optionSize-80)/2 + 25, 5), new Dimension(20, 30), SupplierRow.this,SwingConstants.CENTER, SwingConstants.CENTER);
+		details.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                    }
+                });
+                CustomButton edit = new CustomButton(new ImageIcon("src/GUI/Resources/edit.bin"), "", Color.WHITE, null, false, false, BackGround, true, new Point(idSize + nameSize + emailSize + addressSize + phoneSize + (optionSize-80)/2 + 25, 5), new Dimension(20, 30), SupplierRow.this,SwingConstants.CENTER, SwingConstants.CENTER);
 		edit.setRolloverIcon(new ImageIcon("src/GUI/Resources/editRollover.bin"));
-		CustomButton delete = new CustomButton(new ImageIcon("src/GUI/Resources/delete.bin"), "", Color.WHITE, null, false, false, BackGround, true, new Point(idSize + nameSize + typeSize + addressSize + phoneSize + (optionSize-80)/2 + 48, 5), new Dimension(20, 30), SupplierRow.this,SwingConstants.CENTER, SwingConstants.CENTER);
+		edit.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+                });
+                CustomButton delete = new CustomButton(new ImageIcon("src/GUI/Resources/delete.bin"), "", Color.WHITE, null, false, false, BackGround, true, new Point(idSize + nameSize + emailSize + addressSize + phoneSize + (optionSize-80)/2 + 48, 5), new Dimension(20, 30), SupplierRow.this,SwingConstants.CENTER, SwingConstants.CENTER);
 		delete.setRolloverIcon(new ImageIcon("src/GUI/Resources/deleteRollover.bin"));
-	}
+                delete.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+                });
+        }
 }
--- src/GUI/SupplierManager/SuppliersPanel.java
+++ src/GUI/SupplierManager/SuppliersPanel.java
@@ -1,6 +1,5 @@
 package GUI.SupplierManager;
 
-import GUI.CustomersManager.*;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -44,54 +43,54 @@ public class SuppliersPanel extends JPanel {
         HintTextField search = new HintTextField(" Search Suppliers", CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 12), new Point(d.width - 280, 80), new Dimension(200, 30), SuppliersPanel.this, false);
 
         int totalWidth = d.width - 230;
-        final int nameSize = totalWidth / 3;
-        final int typeSize = totalWidth / 6;
-        final int addressSize = totalWidth / 3;
-        final int phoneSize = totalWidth / 6;
+        final int nameSize = totalWidth / 5;
+        final int emailSize = totalWidth / 15 * 4;
+        final int addressSize = totalWidth / 15 * 4;
+        final int phoneSize = totalWidth / 15 * 4;
         final int idSize = 50;
         final int optionSize = 111;
         Color BACK_GROUND = Color.GRAY;
 
         CustomButton id = new CustomButton("ID", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40, 120), new Dimension(idSize, 30), SuppliersPanel.this);
         CustomButton name = new CustomButton("Name", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + idSize, 120), new Dimension(nameSize, 30), SuppliersPanel.this);
-        CustomButton type = new CustomButton("Address", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + nameSize + idSize, 120), new Dimension(typeSize, 30), SuppliersPanel.this);
-        CustomButton address = new CustomButton("Phone", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + nameSize + typeSize + idSize, 120), new Dimension(addressSize, 30), SuppliersPanel.this);
-        CustomButton phone = new CustomButton("Email", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + nameSize + typeSize + addressSize + idSize, 120), new Dimension(phoneSize, 30), SuppliersPanel.this);
-        CustomButton options = new CustomButton("Options", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + nameSize + typeSize + addressSize + phoneSize + idSize, 120), new Dimension(optionSize, 30), SuppliersPanel.this);
+        CustomButton address = new CustomButton("Address", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + nameSize + idSize, 120), new Dimension(emailSize, 30), SuppliersPanel.this);
+        CustomButton phone = new CustomButton("Phone", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + nameSize + emailSize + idSize, 120), new Dimension(addressSize, 30), SuppliersPanel.this);
+        CustomButton email = new CustomButton("Email", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + nameSize + emailSize + addressSize + idSize, 120), new Dimension(phoneSize, 30), SuppliersPanel.this);
+        CustomButton options = new CustomButton("Options", Color.WHITE, CustomFont.getFont(Configure.DEFAULT_FONT, Font.PLAIN, 15), false, false, BACK_GROUND, true, new Point(40 + nameSize + emailSize + addressSize + phoneSize + idSize, 120), new Dimension(optionSize, 30), SuppliersPanel.this);
 
         final CustomTable table = new CustomTable(new Point(40, 150), new Dimension(d.width - 40, d.height - 40), SuppliersPanel.this);
         JScrollPane x = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         x.setBounds(40, 150, d.width - 70, d.height - 170);
         this.add(x);
         
-        Vector<Customers> loadCustomer = Customers.getAllCustomer();
+//        Vector<Supplier> loadSupplier = null;
  
-        final ArrayList<SuppliersPanel.Customer> customers = new ArrayList<SuppliersPanel.Customer>();
-//        for(int i=0; i<loadCustomer.size(); i++) customers.add(new Customer(loadCustomer.get(i).getCustomerCode() + "", loadCustomer.get(i).getCustomerName(), loadCustomer.get(i).getCustomerType() + "", loadCustomer.get(i).getCustomerAddress() , loadCustomer.get(i).getCustomerPhone()));   
-//        table.setPreferredSize(new Dimension(1000, customers.size() * 40));	
-//        for(int i=0; i<customers.size(); i++)
-//        table.add(new SupplierRow(customers.get(i).id, customers.get(i).name, customers.get(i).type, customers.get(i).address, customers.get(i).phone, idSize, nameSize, typeSize, addressSize, phoneSize, optionSize, new Point(0, i * 40), table));
-        table.setPreferredSize(new Dimension(1000, customers.size() * 40));
+        final ArrayList<SuppliersPanel.Supplier> suppliers = new ArrayList<SuppliersPanel.Supplier>();
+//        for(int i=0; i<loadSupplier.size(); i++) suppliers.add(new Supplier(loadSupplier.get(i).getSupplierCode() + "", loadSupplier.get(i).getSupplierName(), loadSupplier.get(i).getSupplierType() + "", loadSupplier.get(i).getSupplierAddress() , loadSupplier.get(i).getSupplierPhone()));   
+//        table.setPreferredSize(new Dimension(1000, suppliers.size() * 40));	
+//        for(int i=0; i<suppliers.size(); i++)
+//        table.add(new SupplierRow(suppliers.get(i).id, suppliers.get(i).name, suppliers.get(i).email, suppliers.get(i).address, suppliers.get(i).phone, idSize, nameSize, emailSize, addressSize, phoneSize, optionSize, new Point(0, i * 40), table));
+//        table.setPreferredSize(new Dimension(1000, suppliers.size() * 40));
        
         name.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent ae) {
-                        for(int i=0; i<customers.size()-1; i++)
-                            for(int j=i+1; j<customers.size(); j++) 
+                        for(int i=0; i<suppliers.size()-1; i++)
+                            for(int j=i+1; j<suppliers.size(); j++) 
                                 if(nameSort) {
-                                    if(customers.get(i).name.compareTo(customers.get(j).name) < 0) {
-                                        swap(customers, i, j);
+                                    if(suppliers.get(i).name.compareTo(suppliers.get(j).name) < 0) {
+                                        swap(suppliers, i, j);
                                     } 
                                 } else {
-                                    if(customers.get(i).name.compareTo(customers.get(j).name) > 0) {
-                                       swap(customers, i, j);
+                                    if(suppliers.get(i).name.compareTo(suppliers.get(j).name) > 0) {
+                                       swap(suppliers, i, j);
                                     } 
                                 }      
                         nameSort = !nameSort;
                         table.removeAll();
                         SupplierRow.white = true;
-                        for(int i=0; i<customers.size(); i++)
-                	table.add(new MedicineRow(customers.get(i).id, customers.get(i).name, customers.get(i).type, customers.get(i).address, customers.get(i).phone, idSize, nameSize, typeSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
+                        for(int i=0; i<suppliers.size(); i++)
+                	table.add(new MedicineRow(suppliers.get(i).id, suppliers.get(i).name, suppliers.get(i).email, suppliers.get(i).address, suppliers.get(i).phone, idSize, nameSize, emailSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
                         table.repaint();
                     }
                 });
@@ -99,45 +98,45 @@ public class SuppliersPanel extends JPanel {
         id.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent ae) {
-                        for(int i=0; i<customers.size()-1; i++)
-                            for(int j=i+1; j<customers.size(); j++) 
+                        for(int i=0; i<suppliers.size()-1; i++)
+                            for(int j=i+1; j<suppliers.size(); j++) 
                                 if(nameSort) {
-                                    if(customers.get(i).id.compareTo(customers.get(j).id) < 0) {
-                                        swap(customers, i, j);
+                                    if(suppliers.get(i).id.compareTo(suppliers.get(j).id) < 0) {
+                                        swap(suppliers, i, j);
                                     } 
                                 } else {
-                                    if(customers.get(i).id.compareTo(customers.get(j).id) > 0) {
-                                       swap(customers, i, j);
+                                    if(suppliers.get(i).id.compareTo(suppliers.get(j).id) > 0) {
+                                       swap(suppliers, i, j);
                                     } 
                                 }      
                         nameSort = !nameSort;
                         table.removeAll();
                         SupplierRow.white = true;
-                        for(int i=0; i<customers.size(); i++)
-                	table.add(new MedicineRow(customers.get(i).id, customers.get(i).name, customers.get(i).type, customers.get(i).address, customers.get(i).phone, idSize, nameSize, typeSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
+                        for(int i=0; i<suppliers.size(); i++)
+                	table.add(new MedicineRow(suppliers.get(i).id, suppliers.get(i).name, suppliers.get(i).email, suppliers.get(i).address, suppliers.get(i).phone, idSize, nameSize, emailSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
                         table.repaint();
                     }
                 });
         
-        type.addActionListener(new ActionListener() {
+        email.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent ae) {
-                        for(int i=0; i<customers.size()-1; i++)
-                            for(int j=i+1; j<customers.size(); j++) 
+                        for(int i=0; i<suppliers.size()-1; i++)
+                            for(int j=i+1; j<suppliers.size(); j++) 
                                 if(nameSort) {
-                                    if(customers.get(i).type.compareTo(customers.get(j).type) < 0) {
-                                        swap(customers, i, j);
+                                    if(suppliers.get(i).email.compareTo(suppliers.get(j).email) < 0) {
+                                        swap(suppliers, i, j);
                                     } 
                                 } else {
-                                    if(customers.get(i).type.compareTo(customers.get(j).type) > 0) {
-                                       swap(customers, i, j);
+                                    if(suppliers.get(i).email.compareTo(suppliers.get(j).email) > 0) {
+                                       swap(suppliers, i, j);
                                     } 
                                 }      
                         nameSort = !nameSort;
                         table.removeAll();
                         SupplierRow.white = true;
-                        for(int i=0; i<customers.size(); i++)
-                	table.add(new MedicineRow(customers.get(i).id, customers.get(i).name, customers.get(i).type, customers.get(i).address, customers.get(i).phone, idSize, nameSize, typeSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
+                        for(int i=0; i<suppliers.size(); i++)
+                	table.add(new MedicineRow(suppliers.get(i).id, suppliers.get(i).name, suppliers.get(i).email, suppliers.get(i).address, suppliers.get(i).phone, idSize, nameSize, emailSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
                         table.repaint();
                     }
                 });
@@ -145,22 +144,22 @@ public class SuppliersPanel extends JPanel {
         address.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent ae) {
-                        for(int i=0; i<customers.size()-1; i++)
-                            for(int j=i+1; j<customers.size(); j++) 
+                        for(int i=0; i<suppliers.size()-1; i++)
+                            for(int j=i+1; j<suppliers.size(); j++) 
                                 if(nameSort) {
-                                    if(customers.get(i).address.compareTo(customers.get(j).address) < 0) {
-                                        swap(customers, i, j);
+                                    if(suppliers.get(i).address.compareTo(suppliers.get(j).address) < 0) {
+                                        swap(suppliers, i, j);
                                     } 
                                 } else {
-                                    if(customers.get(i).address.compareTo(customers.get(j).address) > 0) {
-                                       swap(customers, i, j);
+                                    if(suppliers.get(i).address.compareTo(suppliers.get(j).address) > 0) {
+                                       swap(suppliers, i, j);
                                     } 
                                 }      
                         nameSort = !nameSort;
                         table.removeAll();
                         SupplierRow.white = true;
-                        for(int i=0; i<customers.size(); i++)
-                	table.add(new MedicineRow(customers.get(i).id, customers.get(i).name, customers.get(i).type, customers.get(i).address, customers.get(i).phone, idSize, nameSize, typeSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
+                        for(int i=0; i<suppliers.size(); i++)
+                	table.add(new MedicineRow(suppliers.get(i).id, suppliers.get(i).name, suppliers.get(i).email, suppliers.get(i).address, suppliers.get(i).phone, idSize, nameSize, emailSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
                         table.repaint();
                     }
                 });
@@ -168,53 +167,53 @@ public class SuppliersPanel extends JPanel {
         phone.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent ae) {
-                        for(int i=0; i<customers.size()-1; i++)
-                            for(int j=i+1; j<customers.size(); j++) 
+                        for(int i=0; i<suppliers.size()-1; i++)
+                            for(int j=i+1; j<suppliers.size(); j++) 
                                 if(nameSort) {
-                                    if(customers.get(i).phone.compareTo(customers.get(j).phone) < 0) {
-                                        swap(customers, i, j);
+                                    if(suppliers.get(i).phone.compareTo(suppliers.get(j).phone) < 0) {
+                                        swap(suppliers, i, j);
                                     } 
                                 } else {
-                                    if(customers.get(i).phone.compareTo(customers.get(j).phone) > 0) {
-                                       swap(customers, i, j);
+                                    if(suppliers.get(i).phone.compareTo(suppliers.get(j).phone) > 0) {
+                                       swap(suppliers, i, j);
                                     } 
                                 }      
                         nameSort = !nameSort;
                         table.removeAll();
                         SupplierRow.white = true;
-                        for(int i=0; i<customers.size(); i++)
-                	table.add(new MedicineRow(customers.get(i).id, customers.get(i).name, customers.get(i).type, customers.get(i).address, customers.get(i).phone, idSize, nameSize, typeSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
+                        for(int i=0; i<suppliers.size(); i++)
+                	table.add(new MedicineRow(suppliers.get(i).id, suppliers.get(i).name, suppliers.get(i).email, suppliers.get(i).address, suppliers.get(i).phone, idSize, nameSize, emailSize, addressSize , phoneSize, optionSize, new Point(0, i * 40), table));
                         table.repaint();
                     }
                 });
     }
 
     public static boolean nameSort = true;
-        public void swap(ArrayList<SuppliersPanel.Customer> customers, int i, int j) {
-            SuppliersPanel.Customer temp = new SuppliersPanel.Customer(customers.get(i).id,customers.get(i).name,customers.get(i).type,customers.get(i).address,customers.get(i).phone);
-            customers.get(i).id = customers.get(j).id;
-            customers.get(i).name = customers.get(j).name;
-            customers.get(i).type = customers.get(j).type;
-            customers.get(i).address = customers.get(j).address;
-            customers.get(i).phone = customers.get(j).phone;
-            customers.get(j).id = temp.id;
-            customers.get(j).name = temp.name;
-            customers.get(j).type = temp.type;
-            customers.get(j).address = temp.address;
-            customers.get(j).phone = temp.phone;
+        public void swap(ArrayList<SuppliersPanel.Supplier> suppliers, int i, int j) {
+            SuppliersPanel.Supplier temp = new SuppliersPanel.Supplier(suppliers.get(i).id,suppliers.get(i).name,suppliers.get(i).email,suppliers.get(i).address,suppliers.get(i).phone);
+            suppliers.get(i).id = suppliers.get(j).id;
+            suppliers.get(i).name = suppliers.get(j).name;
+            suppliers.get(i).email = suppliers.get(j).email;
+            suppliers.get(i).address = suppliers.get(j).address;
+            suppliers.get(i).phone = suppliers.get(j).phone;
+            suppliers.get(j).id = temp.id;
+            suppliers.get(j).name = temp.name;
+            suppliers.get(j).email = temp.email;
+            suppliers.get(j).address = temp.address;
+            suppliers.get(j).phone = temp.phone;
         }
-    public class Customer {
+    public class Supplier {
 
         String id;
         String name;
-        String type;
+        String email;
         String address;
         String phone;
 
-        public Customer(String id, String name, String type, String address, String phone) {
+        public Supplier(String id, String name, String email, String address, String phone) {
             this.id = id;
             this.name = name;
-            this.type = type;
+            this.email = email;
             this.address = address;
             this.phone = phone;
         }
